6a13e8cfef8e127456f04bcd8b717645191a5e56
Rihards Zauls <rihards.zauls@gmail.com>
2019-01-09 12:28:09 +0200

    fixed formatting, (switched linter and some vscode config stuff)

--- script/p5_sketch.js
+++ script/p5_sketch.js
@@ -2,141 +2,139 @@ let time = 0;
 let wave = [];
 let slider;
 
-
 function setup() {
-    createCanvas(800, 600);
-    slider = createSlider(1, 30, 1);
-    slider.position(153, 578);
-    typeSlider = createSlider(1, 4, 1);
-    typeSlider.position(158, 11);
+  createCanvas(800, 600);
+  slider = createSlider(1, 30, 1);
+  slider.position(153, 578);
+  typeSlider = createSlider(1, 4, 1);
+  typeSlider.position(158, 11);
 }
 
-
 function draw() {
-    background(0);
-    translate(400, 300);
+  background(0);
+  translate(400, 300);
 
+  let typeName = "";
+  let x = 0;
+  let y = 0;
+  if (typeSlider.value() === 1) {
+    // square wave
+    typeName = "Square";
+    for (let i = 0; i < slider.value(); i++) {
+      //square series
+      // movement
+      let prevx = x;
+      let prevy = y;
+      let n = i * 2 + 1;
+      let radius = 150 * (4 / (n * PI));
+      x += radius * cos(n * time);
+      y += radius * sin(n * time);
 
-    let typeName = "";
-    let x = 0;
-    let y = 0;
-    if (typeSlider.value() === 1) {
-        // square wave
-        typeName = "Square";
-        for (let i = 0; i < slider.value(); i++) { //square series
-            // movement
-            let prevx = x;
-            let prevy = y;
-            let n = i * 2 + 1;
-            let radius = 150 * (4 / (n * PI));
-            x += radius * cos(n * time);
-            y += radius * sin(n * time);
+      // rings
+      stroke(255, 100);
+      noFill();
+      ellipse(prevx, prevy, radius * 2);
 
-            // rings
-            stroke(255, 100);
-            noFill();
-            ellipse(prevx, prevy, radius * 2);
-
-            // branch
-            stroke(0, 255, 0);
-            line(prevx, prevy, x, y);
-        }
-    } else if (typeSlider.value() === 2) {
-        // sawtooth
-        typeName = "Saw";
-        for (let i = 0; i < slider.value(); i++) { //square series
-            // movement
-            let prevx = x;
-            let prevy = y;
-            let n = i + 1;
-            let radius = 150 * 2 / (Math.pow(-1, n) * n * PI);
-            x += radius * cos(-n * time);
-            y += radius * sin(-n * time);
-
-
-            // rings
-            stroke(255, 100);
-            noFill();
-            ellipse(prevx, prevy, radius * 2);
-
-            // branch
-            stroke(0, 255, 0);
-            line(prevx, prevy, x, y);
-        }
-    } else if (typeSlider.value() === 3) {
-        // reverse sawtooth
-        typeName = "Rev. Saw";
-        for (let i = 0; i < slider.value(); i++) { //square series
-            // movement
-            let prevx = x;
-            let prevy = y;
-            let n = i + 1;
-            let radius = 150 * 2 / (Math.pow(-1, n) * n * PI);
-            x += radius * cos(n * time);
-            y += radius * sin(n * time);
-
-
-            // rings
-            stroke(255, 100);
-            noFill();
-            ellipse(prevx, prevy, radius * 2);
-
-            // branch
-            stroke(0, 255, 0);
-            line(prevx, prevy, x, y);
-        }
-    } else if (typeSlider.value() === 4) {
-        // reverse sawtooth
-        typeName = "Triangle";
-        for (let i = 0; i < slider.value(); i++) { //square series
-            // movement
-            let prevx = x;
-            let prevy = y;
-            let n = 2 * i + 1;
-            let radius = 150 * Math.pow(-1,i) * Math.pow(n,-2);
-            x += radius * cos(n * time);
-            y += radius * sin(n * time);
-
-
-            // rings
-            stroke(255, 100);
-            noFill();
-            ellipse(prevx, prevy, radius * 2);
-
-            // branch
-            stroke(0, 255, 0);
-            line(prevx, prevy, x, y);
-        }
+      // branch
+      stroke(0, 255, 0);
+      line(prevx, prevy, x, y);
     }
+  } else if (typeSlider.value() === 2) {
+    // sawtooth
+    typeName = "Saw";
+    for (let i = 0; i < slider.value(); i++) {
+      //square series
+      // movement
+      let prevx = x;
+      let prevy = y;
+      let n = i + 1;
+      let radius = (150 * 2) / (Math.pow(-1, n) * n * PI);
+      x += radius * cos(-n * time);
+      y += radius * sin(-n * time);
 
-    wave.unshift(y); // add new y value to the start of main array
+      // rings
+      stroke(255, 100);
+      noFill();
+      ellipse(prevx, prevy, radius * 2);
 
-    // take y value and graph on x axis
-    stroke(255, 0, 0, 100);
-    line(x, y, 0, wave[0]); // line from circle to wave
-
-    beginShape();
-    noFill();
-    stroke(255);
-    for (let i = 0; i < wave.length; i++) {
-        vertex(i, wave[i]);
+      // branch
+      stroke(0, 255, 0);
+      line(prevx, prevy, x, y);
     }
-    endShape();
-    // pop the end of array to stop overflow
-    if (wave.length > 500) {
-        wave.pop();
-    }
+  } else if (typeSlider.value() === 3) {
+    // reverse sawtooth
+    typeName = "Rev. Saw";
+    for (let i = 0; i < slider.value(); i++) {
+      //square series
+      // movement
+      let prevx = x;
+      let prevy = y;
+      let n = i + 1;
+      let radius = (150 * 2) / (Math.pow(-1, n) * n * PI);
+      x += radius * cos(n * time);
+      y += radius * sin(n * time);
 
-    // display type of wave
-    fill(255);
-    noStroke();
-    text("Type: " + typeName, -390, -280);
-    fill(255);
-    textSize(20);
-    text("Harmonies: " + slider.value(), -390, 290);
-    // draw coords 
-    stroke(50, 90);
-    line(-380, 0, 380, 0);
-    line(0, 250, 0, -250);
-    time += 0.035; // theta
+      // rings
+      stroke(255, 100);
+      noFill();
+      ellipse(prevx, prevy, radius * 2);
+
+      // branch
+      stroke(0, 255, 0);
+      line(prevx, prevy, x, y);
+    }
+  } else if (typeSlider.value() === 4) {
+    // reverse sawtooth
+    typeName = "Triangle";
+    for (let i = 0; i < slider.value(); i++) {
+      //square series
+      // movement
+      let prevx = x;
+      let prevy = y;
+      let n = 2 * i + 1;
+      let radius = 150 * Math.pow(-1, i) * Math.pow(n, -2);
+      x += radius * cos(n * time);
+      y += radius * sin(n * time);
+
+      // rings
+      stroke(255, 100);
+      noFill();
+      ellipse(prevx, prevy, radius * 2);
+
+      // branch
+      stroke(0, 255, 0);
+      line(prevx, prevy, x, y);
+    }
+  }
+
+  wave.unshift(y); // add new y value to the start of main array
+
+  // take y value and graph on x axis
+  stroke(255, 0, 0, 100);
+  line(x, y, 0, wave[0]); // line from circle to wave
+
+  beginShape();
+  noFill();
+  stroke(255);
+  for (let i = 0; i < wave.length; i++) {
+    vertex(i, wave[i]);
+  }
+  endShape();
+  // pop the end of array to stop overflow
+  if (wave.length > 500) {
+    wave.pop();
+  }
+
+  // display type of wave
+  fill(255);
+  noStroke();
+  text("Type: " + typeName, -390, -280);
+  fill(255);
+  textSize(20);
+  text("Harmonies: " + slider.value(), -390, 290);
+  // draw coords
+  stroke(50, 90);
+  line(-380, 0, 380, 0);
+  line(0, 250, 0, -250);
+  time += 0.035; // theta
 }
--- script/p5_sketch_3d.js
+++ script/p5_sketch_3d.js
@@ -1,31 +1,31 @@
 let time = 0;
 
 function setup() {
-    createCanvas(1000, 800, WEBGL);
+  createCanvas(1000, 800, WEBGL);
 }
 
 function draw() {
-    background(200);
-    orbitControl();
+  background(200);
+  orbitControl();
 
-    // x axis (red)
-    stroke(255, 0, 0);
-    line(0, 0, 0, 400, 0, 0);
+  // x axis (red)
+  stroke(255, 0, 0);
+  line(0, 0, 0, 400, 0, 0);
 
-    // y axis (green)
-    stroke(0, 255, 0);
-    line(0, 0, 0, 0, -400, 0);
+  // y axis (green)
+  stroke(0, 255, 0);
+  line(0, 0, 0, 0, -400, 0);
 
-    // z axis (blue)
-    stroke(0, 0, 255);
-    line(0, 0, 0, 0, 0, 400);
+  // z axis (blue)
+  stroke(0, 0, 255);
+  line(0, 0, 0, 0, 0, 400);
 
-    noFill();
-    stroke(255);
-    sphere(200);
-    fill(150);
-    box(100);
-    rotateX(frameCount * 0.01);
+  noFill();
+  stroke(255);
+  sphere(200);
+  fill(150);
+  box(100);
+  rotateX(frameCount * 0.01);
 
-    time += 1
+  time += 1;
 }
